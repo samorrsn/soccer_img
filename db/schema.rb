451808fd2ccf38467405ca3103bc2a2d57dc9511
Mark Berry <markberry867@gmail.com>
2014-11-03 04:59:13 -0600

    Updated Positions Model in db, changed name to position_name, removed user_id and team_id attributes and added team_member_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141103104721) do
+ActiveRecord::Schema.define(version: 20141103105308) do
 
   create_table "positions", force: true do |t|
-    t.string   "name"
-    t.integer  "user_id"
-    t.integer  "team_id"
+    t.string   "position_name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "team_member_id"
   end
 
   create_table "team_members", force: true do |t|
